f6ac5e70cb4d5224db61b91f533effbd89f3fc7b
Eric Villnow <villnoweric@gmail.com>
2022-04-24 12:02:37 -0500

    Fixed broken Links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,11 +14,11 @@
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
               
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="/" class="nav-link px-2 text-white">Home</a></li>
-                <li><a href="/about" class="nav-link px-2 text-white">About</a></li>
+                <li><a href="{{ url('/') }}" class="nav-link px-2 text-white">Home</a></li>
+                <li><a href="{{ url('/about') }}/about" class="nav-link px-2 text-white">About</a></li>
             </ul>
     
-            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" action="search" method="get">
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" action="{{ url('/search') }}" method="get">
                 <input name="sq" type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
             </form>
     
